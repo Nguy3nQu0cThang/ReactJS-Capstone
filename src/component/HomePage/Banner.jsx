89cfa4c144564589ef6962e83b3629a02dabdc0a
TuTrieuDuong <tutrieuduong98@gmail.com>
2025-04-08 15:43:15 +0700

    cài tailwindcss

--- src/component/HomePage/Banner.jsx
+++ src/component/HomePage/Banner.jsx
@@ -2,32 +2,38 @@ import { useEffect, useState } from "react";
 import { http, TOKEN_CYBERSOFT } from "../../utils/setting";
 
 const Banner = () => {
-  const [banners, setBanners] = useState([])
+  const [banners, setBanners] = useState([]);
 
   useEffect(() => {
     const getAPIQuanLyPhimBanner = async () => {
       try {
-        const res = await http.get(
-          "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachBanner",
-        );
-        setBanners(res.data.content)
+        const res = await http.get("/api/QuanLyPhim/LayDanhSachBanner");
+        setBanners(res.data.content);
       } catch (err) {
         console.log("Error Banner", err);
       }
     };
-    getAPIQuanLyPhimBanner()
-  },[])
+    getAPIQuanLyPhimBanner();
+  }, []);
 
   return (
     <div
-      id="carouselExampleSlidesOnly"
+      id="carouselExampleInterval"
       className="carousel slide"
       data-bs-ride="carousel"
     >
       <div className="carousel-inner">
         {banners.map((banner, index) => (
-          <div className={`carousel-item ${index===0 ? 'active' : ''}`} key={index}>
-            <img src={banner.hinhAnh} className="d-block w-100 h-50" alt="Banner" />
+          <div
+            className={`carousel-item ${index === 0 ? "active" : ""}`}
+            key={banner.maBanner}
+            data-bs-interval={index === 0 ? 5000 : 3000}
+          >
+            <img
+              src={banner.hinhAnh}
+              className="d-block w-100"
+              alt={`bannerr ${banner.maBanner}`}
+            />
           </div>
         ))}
       </div>
